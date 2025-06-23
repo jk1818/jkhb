d928560f40bb015270c24c9349dcc20e0c5f6e5e
baidu <vip@baidu.com>
2025-06-24 00:45:19 +0800

    up

--- running/runzb.py
+++ running/runzb.py
@@ -1,5 +1,5 @@
 from main import multicast_province
-config_files = ["data/四川_config.txt", "data/湖北_config.txt"]
+config_files = ["data/四川电信_config.txt", "data/湖北电信_config.txt"]
 for config_file in config_files:
     multicast_province(config_file)
 print(f"组播地址获取完成")
