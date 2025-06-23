d155e8945204010b4e5d41ccf78e3ffd381a47c3
baidu <web@baidu.com>
2025-06-23 19:58:03 +0800

    up

--- running/runzb.py
+++ running/runzb.py
@@ -1,4 +1,4 @@
-from zubo import multicast_province
+from main import multicast_province
 config_files = ["data/湖北_config.txt", "data/四川_config.txt"]
 for config_file in config_files:
     multicast_province(config_file)
